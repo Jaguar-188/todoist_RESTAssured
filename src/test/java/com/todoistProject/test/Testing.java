fecbe8e0755f7820eb664b56209118d6ab47797c
Jaguar-188 <ruturajlokhande9@gmail.com>
2021-08-20 10:58:04 +0530

    added create task endpoint testing

--- src/test/java/com/todoistProject/test/Testing.java
+++ src/test/java/com/todoistProject/test/Testing.java
@@ -1,26 +1,54 @@
 package com.todoistProject.test;
 
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class Testing {
 	
+	String token = "a8c484f6ab78bb5d8943ed56791c4ce57becff5e";
+	String url = "https://api.todoist.com/rest/v1/";
+//	@Test
+//	public void testToFetchData() {
+//		
+//		Response resp = RestAssured.given().header("Authorization","Bearer "+token).get(url+"projects");
+//		
+//		System.out.println(resp.getStatusCode());
+//		System.out.println(resp.getTime());
+//		System.out.println(resp.getBody().asString());
+//
+//		Assert.assertEquals(resp.getStatusCode(), 200);
+//	}
 	
 	@Test
-	public void testToFetchData() {
+	public void testToCheckCreateTask() {
 		
-		String token = "a8c484f6ab78bb5d8943ed56791c4ce57becff5e";
+		JSONObject jsonObject = new JSONObject();
 		
-		Response resp = RestAssured.given().header("Authorization","Bearer "+token).get("https://api.todoist.com/rest/v1/projects");
+		jsonObject.put("content","Buy Water");
+		jsonObject.put("due_string","tomorrow at 11:00");
+		jsonObject.put("due_lang","en");
+		jsonObject.put("priority",3);
 		
+		RequestSpecification request =  (RequestSpecification) RestAssured.given()
+						.header("Authorization","Bearer "+token)
+						.header("Content-Type","application/json")
+						.body(jsonObject.toString())
+						.when();
+						
+		
+		Response resp = request.post(url+"tasks");
 		System.out.println(resp.getStatusCode());
 		System.out.println(resp.getTime());
 		System.out.println(resp.getBody().asString());
 
 		Assert.assertEquals(resp.getStatusCode(), 200);
+		
 	}
+	
 
 }
